0e9ac7ade67397477d2a3725511730053d3d75b9
Federica <147710098+JurinS0@users.noreply.github.com>
2024-03-21 09:32:12 +0100

    Update ThreadGestioneServizioChat.java

--- ThreadGestioneServizioChat.java
+++ ThreadGestioneServizioChat.java
@@ -3,9 +3,11 @@ import java.net.*;
 import java.util.*;
 import javax.swing.JOptionPane;
 
-public class ThreadGestioneServizioChat implements Runnable {
-    private int nMaxConnessioni;
-    private List<ThreadChatConnessioni> lista;
+//questa classe serve per gestire i server e le connessioni dei client ad essi
+
+public class ThreadGestioneServizioChat implements Runnable { //questo thread si occupa di ricevere le connessioni dei client
+    private int nMaxConnessioni;//numero max connessioni 
+    private List<ThreadChatConnessioni> lista;//lista che conterrà le connessioni che in quel momento sono attive
     private Thread[] listaThreadConnessioni;
     private Thread me;
     private ServerSocket serverChat;
@@ -22,13 +24,13 @@ public class ThreadGestioneServizioChat implements Runnable {
         boolean continua = true;
 
         try {
-            serverChat = new ServerSocket(9999);
+            serverChat = new ServerSocket(9999);//rimane in attesa di connessioni da parte dei client
             System.out.println("Server attivo");
         } catch (IOException e) {
             System.out.println("Errore nella creazione del server");
             continua = false;
         }
-
+//se l'operazione di prima va a buon fine si crea un nuovo thred che conterra ogni connessione
         if (continua) {
             try {
                 for (int i = 0; i < nMaxConnessioni; i++) {
@@ -45,4 +47,15 @@ public class ThreadGestioneServizioChat implements Runnable {
             }
         }
     }
-}
+
+    public void spedisciMessaggio(String mes){
+        //scrittura del messaggio
+        lista.add(mes);
+        lista.select(lista.getItemCount()-1);
+//invio del messaggio a tutti i client
+        for(int xx=0; xx<this.nrMaxCknnessioni; xx++){
+            if(listaConnessioni[xx] != null){
+                listaConnessioni[xx].spedisciMessaggioChat(mes);
+       }
+   }
+    }
