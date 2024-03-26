6914edc18612188805153651d68d6fab2befd187
Federica <147710098+JurinS0@users.noreply.github.com>
2024-03-26 17:00:50 +0100

    Update ThreadChatClient.java

--- ThreadChatClient.java
+++ ThreadChatClient.java
@@ -3,10 +3,9 @@ import java.io.*;
 import java.net.*;
 import javax.swing.*;
 
-//questa classe rappresenta l'implementazione del servizio client della chat
-//in questo caso il client ha solo il compito di connetersi con il server e rimanere in attesa 
-//dei messaggi inviati dal servere e su richiesta dell'utente deve inviare messaggi verso il server
-
+//questa classe rappresenta l'implementazione del servizio client della chat.
+//in questo caso, il client ha solo il compito di connettersi con il server e rimanere in attesa 
+//dei messaggi inviati dal server e, su richiesta dell'utente, deve inviare messaggi verso il server.
 public class ThreadChatClient implements Runnable {
     private List lista;
     private Thread me;
@@ -27,12 +26,12 @@ public class ThreadChatClient implements Runnable {
         me.start();
     }
 
-    public void run() { //gestisce la ricezione dei messaggi da parte del server
+    public void run() {//gestisce la ricezione dei messaggi da parte del server
         try {
             while (true) {
                 String mes = input.readLine();
                 if (mes != null) {
-                    lista.add(mes); //i messaggi vengono inseriti in una lista 
+                    lista.add(mes);//i messaggi vengono inseriti in una lista
                     lista.select(lista.getItemCount() - 1);
                 }
             }
@@ -41,7 +40,7 @@ public class ThreadChatClient implements Runnable {
         }
     }
 
-    public void spedisciMessaggio(String messaggio) { //utilizzato per spedire messaggi al server
+    public void spedisciMessaggio(String messaggio) {//utilizzato per spedire messaggi al server
         output.println(messaggio);
     }
 }
