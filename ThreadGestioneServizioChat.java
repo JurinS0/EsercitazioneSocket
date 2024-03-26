9eb11c38d2c078643331a9d90b76a489e0914e69
Federica <147710098+JurinS0@users.noreply.github.com>
2024-03-26 20:24:17 +0100

    Update ThreadGestioneServizioChat.java

--- ThreadGestioneServizioChat.java
+++ ThreadGestioneServizioChat.java
@@ -1,20 +1,20 @@
 import java.io.*;
 import java.net.*;
-import java.util.*;
+import java.awt.*;
 import javax.swing.JOptionPane;
 
 //questa classe serve per gestire i server e le connessioni dei client ad essi
 
 class ThreadGestioneServizioChat implements Runnable {//questo thread si occupa di ricevere le connessioni dei client
-    private int nMaxConnessioni;
-    private List<String> lista;
-    private Thread[] listaThreadConnessioni;
+    private int nMaxConnessioni;//numero max connessioni
+    private List lista;
+    private Thread[] listaThreadConnessioni;//lista che conterrà le connessioni che in quel momento sono attive
     private Thread me;
     private ServerSocket serverChat;
 
-    public ThreadGestioneServizioChat(int nMaxConnessioni, List<String> lista) {
-        this.nMaxConnessioni = nMaxConnessioni;//numero max connessioni
-        this.lista = lista;//lista che conterrà le connessioni che in quel momento sono attive
+    public ThreadGestioneServizioChat(int nMaxConnessioni, List lista) {
+        this.nMaxConnessioni = nMaxConnessioni;
+        this.lista = lista;
         this.listaThreadConnessioni = new Thread[nMaxConnessioni];
         this.me = new Thread(this);
         this.me.start();
@@ -27,7 +27,7 @@ class ThreadGestioneServizioChat implements Runnable {//questo thread si occupa 
             serverChat = new ServerSocket(9999);//rimane in attesa di connessioni da parte dei client
             System.out.println("Server attivo");
         } catch (IOException e) {
-            System.out.println("Errore nella creazione del server");
+            System.out.println("Errore nella creazione del server: " + e.getMessage());
             continua = false;
         }
 //se l'operazione di prima va a buon fine si crea un nuovo thred che conterra ogni connessione
@@ -39,25 +39,24 @@ class ThreadGestioneServizioChat implements Runnable {//questo thread si occupa 
                     Thread threadConnessione = new Thread(nuovaConnessione);
                     listaThreadConnessioni[i] = threadConnessione;
                     threadConnessione.start();
-                }
+
+}
                 serverChat.close();
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "Impossibile creare la connessione");
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "Impossibile creare la connessione: " + e.getMessage());
             }
         }
     }
 
-    public void spedisciMessaggio(String mes) {
-    lista.add(mes); //aggiunge il messaggio alla lista dei messaggi visualizzati sul server
-
-    //invio del messaggio a tutti
-    for (int i = 0; i < listaThreadConnessioni.length; i++) {
-        Thread thread = listaThreadConnessioni[i];
-        if (thread != null) {
-            ThreadChatConnessioni connessione = (ThreadChatConnessioni) thread;
-            connessione.spedisciMessaggio(mes);
+    public synchronized void spedisciMessaggio(String mes) {
+        lista.add(mes);//aggiunge il messaggio alla lista dei messaggi visualizzati sul server
+//invio del messaggio a tutti
+        for (int i = 0; i < listaThreadConnessioni.length; i++) {
+            ThreadChatConnessioni thread = ((ThreadChatConnessioni) (listaThreadConnessioni[i]));
+            if (thread != null) {
+                ThreadChatConnessioni connessione = thread;
+                connessione.spedisciMessaggio(mes);
+            }
         }
     }
-}
-
 }
