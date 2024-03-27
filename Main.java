b5d875046100cb1fe5be98217737e067ba981433
Federica <147710098+JurinS0@users.noreply.github.com>
2024-03-27 10:23:40 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 
-public class Main {
+public class MainServer {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
